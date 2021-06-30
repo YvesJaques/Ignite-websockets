757e00082fa629acddb28fdb6a7b675545a1b1cc
Yves Morais Jaques <ymoraisjaques@gmail.com>
2021-06-30 10:47:31 -0300

    fix: Get User by socket service return type

--- src/services/GetUserBySocketIdService.ts
+++ src/services/GetUserBySocketIdService.ts
@@ -4,7 +4,7 @@ import { User } from '../schemas/User';
 
 injectable();
 class GetUserBySocketIdService {
-  async execute(socket_id: string) {
+  async execute(socket_id: string): Promise<User> {
     const user = await User.findOne({ socket_id });
     return user;
   }
