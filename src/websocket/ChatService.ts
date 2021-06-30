65a7679983a2485f760bbff2e9fe6c4f6feae49a
Yves Morais Jaques <ymoraisjaques@gmail.com>
2021-06-30 10:38:01 -0300

    feat: User chat room creation

--- src/websocket/ChatService.ts
+++ src/websocket/ChatService.ts
@@ -4,6 +4,7 @@ import { io } from '../http';
 import { CreateChatRoomService } from '../services/CreateChatRoomService';
 import { CreateUserService } from '../services/CreateUserService';
 import { GetAllUsersService } from '../services/GetAllUsersService';
+import { GetChatRoomByUsersService } from '../services/GetChatRoomByUsersService';
 import { GetUserBySocketIdService } from '../services/GetUserBySocketIdService';
 
 io.on('connect', (socket) => {
@@ -30,12 +31,23 @@ io.on('connect', (socket) => {
 
   socket.on('start_chat', async (data, callback) => {
     const createChatRoomService = container.resolve(CreateChatRoomService);
+    const getChatRoomByUsersService = container.resolve(GetChatRoomByUsersService);
     const getUserBySocketId = container.resolve(GetUserBySocketIdService);
 
     const userLogged = await getUserBySocketId.execute(socket.id);
 
-    const room = await createChatRoomService.execute([data.idUser, userLogged._id]);
+    let room = await getChatRoomByUsersService.execute([
+      data.idUser,
+      userLogged.id,
+    ]);
 
-    callback(room);
+    if (!room) {
+      room = await createChatRoomService.execute([
+        data.idUser,
+        userLogged._id,
+      ]);
+    }
+
+    callback({ room });
   });
 });
